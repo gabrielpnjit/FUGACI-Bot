6791e2f2d7bc5ac306f6cb31fa8a563004cd7f2d
Gabriel Pascual <gzp2@njit.edu>
2022-08-01 20:32:11 -0400

    completed command handler

--- commands/mock-get-clan-elo.js
+++ commands/mock-get-clan-elo.js
@@ -3,11 +3,11 @@ const bhapi = require('../functions.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('leaderboard')
-        .setDescription('View ranked leaderboard of all FUGACI members by peak ELO!'),
+        .setName('mockleaderboard')
+        .setDescription('View mock ranked leaderboard of all FUGACI members by peak ELO!'),
     async execute(interaction) {
         await interaction.deferReply();
-        const res = await bhapi.getClanElo();
+        const res = await bhapi.mockGetClanElo();
         if (res) {
             console.log(res);
             interaction.editReply(JSON.stringify(res));
--- delete-commands.js
+++ delete-commands.js
@@ -8,14 +8,6 @@ const TOKEN = process.env.DISC_TOKEN;
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 // for guild-based commands, replace third parameter with command id and run this to delete command
-rest.delete(Routes.applicationGuildCommand(CLIENTID, GUILDID, '1003108063856820274'))
-	.then(() => console.log('Successfully deleted guild command'))
-	.catch(console.error);
-
-rest.delete(Routes.applicationGuildCommand(CLIENTID, GUILDID, '1003108063856820275'))
-	.then(() => console.log('Successfully deleted guild command'))
-	.catch(console.error);
-
-rest.delete(Routes.applicationGuildCommand(CLIENTID, GUILDID, '1003108063856820276'))
+rest.delete(Routes.applicationGuildCommand(CLIENTID, GUILDID, '1003140620262580295'))
 	.then(() => console.log('Successfully deleted guild command'))
 	.catch(console.error);

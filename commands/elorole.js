a31e8d18595d469ac3a39159414f46b858742003
Gabriel Pascual <gzp2@njit.edu>
2022-08-20 15:39:58 -0400

    Basic elo role request

--- commands/elorole.js
+++ commands/elorole.js
@@ -5,6 +5,30 @@ module.exports = {
         .setName('elorole')
         .setDescription('Send a request to update peak elo role! A'),
     async execute(interaction) {
-        await interaction.reply('Send a request to update your peak elo role!');
+        const modal = new ModalBuilder()
+            .setCustomId('eloRole')
+            .setTitle('Update Peak Elo Role!');
+        const nameInput = new TextInputBuilder()
+            .setCustomId('eloName')
+            .setLabel('In-Game Name')
+            .setMaxLength(32)
+            .setStyle(TextInputStyle.Short);
+        const eloInput = new TextInputBuilder()
+            .setCustomId('eloClaim')
+            .setLabel('Peak Elo')
+            .setMaxLength(4)
+            .setStyle(TextInputStyle.Short);
+        const eloImage = new TextInputBuilder()
+            .setCustomId('eloImage')
+            .setLabel('Image URL of Elo')
+            .setMaxLength(1000)
+            .setStyle(TextInputStyle.Short);
+        const firstActionRow = new ActionRowBuilder().addComponents(nameInput);
+        const secondActionRow = new ActionRowBuilder().addComponents(eloInput);
+        const thirdActionRow = new ActionRowBuilder().addComponents(eloImage);
+
+        modal.addComponents(firstActionRow, secondActionRow, thirdActionRow);
+
+        await interaction.showModal(modal);
     },
 };
---
+++ modals/elo-role.js
@@ -0,0 +1,43 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: {
+        name: 'eloRole',
+    },
+    async execute(interaction) {
+        try {
+            await interaction.deferReply({ ephemeral: true });
+            const channel = await interaction.client.channels.cache.get('1010626707880935484');
+            const name = interaction.fields.getTextInputValue('eloName');
+            const elo = interaction.fields.getTextInputValue('eloClaim');
+            const image = interaction.fields.getTextInputValue('eloImage');
+            const author = interaction.user.tag;
+            const avatarUrl = interaction.user.displayAvatarURL();
+            const guild = interaction.guild;
+            const iconUrl = interaction.guild.iconURL();
+
+            const embed = new EmbedBuilder()
+                .setColor('#00FFFF')
+                .setTitle('Elo Request')
+                .setThumbnail('https://cdn.discordapp.com/attachments/756654864280453134/1010632673926058104/Logo_BrawlhallaHammer.png')
+                .setAuthor({ name: `${author}`, iconURL: avatarUrl })
+                .addFields(
+                    { name: 'Name', value: name },
+                    { name: 'Elo Claim', value: elo },
+                    { name: 'Image', value: image },
+                )
+                .setImage(image)
+                .setFooter({ text: `Discord Server: ${guild}`, iconURL: iconUrl })
+                .setTimestamp();
+
+            await channel.send({ embeds: [embed] });
+            console.log('Elo Request was successfully submitted!');
+            await interaction.editReply({ content: 'Successfully submitted elo role request! A moderator will review your request shortly. Thank you!', ephemeral: true });
+        }
+        catch (error) {
+            await interaction.editReply({ content: 'Invalid image URL! Please try again!', ephemeral: true });
+            console.log('Error with sending elo role request:');
+            console.log(error);
+        }
+    },
+};

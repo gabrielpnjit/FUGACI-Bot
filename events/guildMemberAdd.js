214ab7ae6f3482f5e74b652c589af1173de63096
Gabriel Pascual <gzp2@njit.edu>
2023-01-08 01:04:38 -0500

    adjusted welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,19 @@
 const { EmbedBuilder } = require('discord.js');
 
+function getOrdinal(n) {
+    let ord = 'th';
+    if (n % 10 == 1 && n % 100 != 11) {
+      ord = 'st';
+    }
+    else if (n % 10 == 2 && n % 100 != 12) {
+      ord = 'nd';
+    }
+    else if (n % 10 == 3 && n % 100 != 13) {
+      ord = 'rd';
+    }
+    return ord;
+  }
+
 module.exports = {
     name: 'guildMemberAdd',
     execute(member) {
@@ -12,13 +26,12 @@ module.exports = {
         const welcomeEmbed = new EmbedBuilder()
         .setTitle(`Welcome to FUGACI!`)
         .setAuthor({ name: `${user.tag}`, iconURL: `${user.displayAvatarURL()}` })
-        .setDescription(`╔════════════╗\nMake sure to check out:\n${rulesChannel}\n${tryoutsChannel}\n${rolesChannel}\n╚════════════╝`)
+        .setDescription(`Hello ${member}!\nYou are our ${guild.memberCount}${getOrdinal(guild.memberCount)} Member!\n╔════════════╗\nMake sure to check out:\n${rulesChannel}\n${tryoutsChannel}\n${rolesChannel}\n╚════════════╝`)
         .setColor('#5FA052')
         // .setFooter({ text: `Member #${guild.memberCount}` })
         .setTimestamp()
         .setThumbnail('https://cdn.discordapp.com/attachments/689908352079495221/1061365775492976741/EMBER_Fangwilds_Heart_Ember_Classic_Colors_Emote_See_Ya_5_242x350.png');
         welcomeChannel.send({
-            content: `Hello ${member}!`,
             embeds: [welcomeEmbed],
         });
     },

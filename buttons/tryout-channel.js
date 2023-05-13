837ce2e0c94b60e007ff8ebf982784e92b43d96c
Gabriel Pascual <gzp2@njit.edu>
2023-05-12 21:33:08 -0400

    added role restrictions for tryout buttons

---
+++ buttons/tryout-channel.js
@@ -0,0 +1,16 @@
+module.exports = {
+    data: {
+        name: 'createTryoutChannel',
+    },
+    async execute(interaction) {
+        await interaction.deferReply();
+        const ownerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Clan Owner');
+        const coOwnerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Clan Co-Owner');
+        const officerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Officer');
+        if (!(interaction.member.roles.cache.has(ownerRole.id) || interaction.member.roles.cache.has(coOwnerRole.id) || interaction.member.roles.cache.has(officerRole.id))) {
+            await interaction.editReply({ content: 'You do not have permission to do that!', ephemeral: true });
+            return;
+        }
+        await interaction.editReply({ content: 'Created Channel Successfully!', ephemeral: true });
+    },
+};

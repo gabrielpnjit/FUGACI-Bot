1ee5a9670ba34d5389e88444bacdbae34859a095
Gabriel Pascual <gzp2@njit.edu>
2023-05-13 15:47:27 -0400

    reject and accept button functionality added

--- buttons/tryout-accept.js
+++ buttons/tryout-accept.js
@@ -3,14 +3,26 @@ module.exports = {
         name: 'tryoutAccept',
     },
     async execute(interaction) {
-        await interaction.deferReply();
+        await interaction.deferReply({ ephemeral: true });
         const ownerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Clan Owner');
         const coOwnerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Clan Co-Owner');
         const officerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Officer');
+        const memberRole = interaction.message.guild.roles.cache.find(role => role.name === 'Member');
+
+        // Allow only officers, owners, and coowners to run these buttons
         if (!(interaction.member.roles.cache.has(ownerRole.id) || interaction.member.roles.cache.has(coOwnerRole.id) || interaction.member.roles.cache.has(officerRole.id))) {
             await interaction.editReply({ content: 'You do not have permission to do that!', ephemeral: true });
             return;
         }
+
+        const reply = await interaction.fetchReply();
+        const id = reply.reference.messageId;
+        const message = await interaction.message.fetch(`${id}`);
+        const userId = message.embeds[0].data.footer.text.slice(9);
+        const member = await interaction.message.guild.members.fetch(userId);
+
+        await member.roles.add(memberRole);
+        await interaction.client.users.send(userId, 'Your tryout for FUGACI has been accepted!');
         await interaction.editReply({ content: 'Accepted Successfully!', ephemeral: true });
     },
 };
--- buttons/tryout-reject.js
+++ buttons/tryout-reject.js
@@ -3,14 +3,23 @@ module.exports = {
         name: 'tryoutReject',
     },
     async execute(interaction) {
-        await interaction.deferReply();
+        await interaction.deferReply({ ephemeral: true });
         const ownerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Clan Owner');
         const coOwnerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Clan Co-Owner');
         const officerRole = interaction.message.guild.roles.cache.find(role => role.name === 'Officer');
+
+        // Allow only officers, owners, and coowners to run these buttons
         if (!(interaction.member.roles.cache.has(ownerRole.id) || interaction.member.roles.cache.has(coOwnerRole.id) || interaction.member.roles.cache.has(officerRole.id))) {
             await interaction.editReply({ content: 'You do not have permission to do that!', ephemeral: true });
             return;
         }
+
+        const reply = await interaction.fetchReply();
+        const id = reply.reference.messageId;
+        const message = await interaction.message.fetch(`${id}`);
+        const userId = message.embeds[0].data.footer.text.slice(9);
+
+        await interaction.client.users.send(userId, 'Your tryout for FUGACI has been rejected.');
         await interaction.editReply({ content: 'Rejected Successfully!', ephemeral: true });
     },
 };

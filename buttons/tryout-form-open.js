4b54e62f27a0cc5ad4644ef5b59b71cc405e9556
Gabriel Pascual <gzp2@njit.edu>
2023-05-12 20:08:11 -0400

    tryout form created

--- buttons/tryout-form-open.js
+++ buttons/tryout-form-open.js
@@ -1,10 +1,51 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 
 module.exports = {
     data: {
         name: 'tryoutOpen',
     },
     async execute(interaction) {
-        await interaction.reply('clicked');
+        const modal = new ModalBuilder()
+            .setCustomId('tryoutSubmit')
+            .setTitle('FUGACI Tryout Form');
+        const nameInput = new TextInputBuilder()
+            .setCustomId('tryoutIgn')
+            .setLabel('In-Game Name')
+            .setMaxLength(32)
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
+        const eloInput = new TextInputBuilder()
+            .setCustomId('tryoutElo')
+            .setLabel('Peak ELO')
+            .setMaxLength(4)
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
+        const hourInput = new TextInputBuilder()
+            .setCustomId('tryoutHours')
+            .setLabel('Brawlhalla Playtime (Hours)')
+            .setMaxLength(10)
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
+        const mainInput = new TextInputBuilder()
+            .setCustomId('tryoutMain')
+            .setLabel('Main Legend')
+            .setMaxLength(32)
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
+        const bodyInput = new TextInputBuilder()
+            .setCustomId('tryoutReason')
+            .setLabel('Why would you like to tryout for FUGACI?')
+            .setMaxLength(1000)
+            .setStyle(TextInputStyle.Paragraph)
+            .setRequired(true);
+        const firstActionRow = new ActionRowBuilder().addComponents(nameInput);
+        const secondActionRow = new ActionRowBuilder().addComponents(eloInput);
+        const thirdActionRow = new ActionRowBuilder().addComponents(hourInput);
+        const fourthActionRow = new ActionRowBuilder().addComponents(mainInput);
+        const fifthActionRow = new ActionRowBuilder().addComponents(bodyInput);
+
+        modal.addComponents(firstActionRow, secondActionRow, thirdActionRow, fourthActionRow, fifthActionRow);
+
+        await interaction.showModal(modal);
     },
 };
